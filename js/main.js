10d265c93a53e865f2909795dd19ac303bc8009b
Ben Bristow <me@benbristow.co.uk>
2013-08-15 19:58:46 +0100

    Prevent page refresh

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ function update() {
 	//Split into character array
 	var chars = memorableinfo.split('');
 
+	//Loop trough character array and generate HTML
 	var html = "";
 
 	$.each(chars, function(index, value) {
@@ -12,6 +13,8 @@ function update() {
 		html = html + "<strong> " + (index + 1) + "</strong>: " + value;
 	});
 
+	//Show outcome
 	$("#outcome").html(html);
 	$("#selection").fadeIn(500);
+
 }
